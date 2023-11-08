1a3e7f45a9aa4291ecf36f65cd4a7199f9aa4a94
imdkhairul <imdkhairul@gmail.com>
2023-11-08 07:44:22 +0600

    Update AccountView.swift

--- Screens/AccountView/AccountView.swift
+++ Screens/AccountView/AccountView.swift
@@ -10,14 +10,34 @@ import SwiftUI
 struct AccountView: View {
    
     @StateObject var viewModel:AccountViewModel = AccountViewModel()
+    @FocusState private var focusTextField:FormTextField?
+    
+    enum FormTextField {
+        case firstName, lastNme, email
+    }
     
     var body: some View {
         NavigationView{
             Form{
                 Section(header:Text("Personal Info")){
                     TextField("First Name", text: $viewModel.firstName)
+                        .focused($focusTextField,equals: .firstName)
+                        .submitLabel(.next)
+                        .onSubmit {
+                            focusTextField = .lastNme
+                        }
                     TextField("Last Name", text: $viewModel.lastName)
+                        .focused($focusTextField,equals: .lastNme)
+                        .submitLabel(.next)
+                        .onSubmit {
+                            focusTextField = .email
+                        }
                     TextField("Email", text: $viewModel.email)
+                        .focused($focusTextField,equals: .email)
+                        .submitLabel(.continue)
+                        .onSubmit {
+                            focusTextField = nil
+                        }
                         .keyboardType(.emailAddress)
                         .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
                         .autocorrectionDisabled()
